2f6709809e41bf28174f799d501f8388ccfa8315
yuuta-git12 <tayu2201@icloud.com>
2024-12-01 12:35:49 +0900

    ルート定義を分割

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
-import { BrowserRouter,Link,Switch,Route } from 'react-router-dom/cjs/react-router-dom'
+import { BrowserRouter,Link } from 'react-router-dom/cjs/react-router-dom'
+
+import { Router } from './router/Router'
 
 import './App.css'
-import { Home } from './Home'
-import { Page1 } from './Page1'
-import { Page2 } from './Page2'
-import { Page1DetailA } from './Page1DetailA'
-import { Page1DetailB } from './Page1DetailB'
 
 function App() {
   
@@ -24,29 +21,7 @@ function App() {
           <Link to="/page2">Page2</Link>
 
         </div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/page1" render={({match: {url}}) => (
-            <Switch>
-              <Route exact path={url}>
-                <Page1 />
-              </Route>
-              <Route path={`${url}/detailA`}>
-                <Page1DetailA />
-              </Route>
-              <Route path={`${url}/detailB`}>
-                <Page1DetailB />
-              </Route>
-            </Switch>    
-            )}
-          />
-          
-          <Route path="/page2">
-            <Page2 />
-          </Route>
-        </Switch>
+        <Router />
 
       </BrowserRouter>
     </>
