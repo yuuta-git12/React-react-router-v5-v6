b75eaf3b8a10aa2e34678da7f5dd9380a8fb7719
yuuta-git12 <tayu2201@icloud.com>
2024-12-01 19:01:19 +0900

    Linkを使わないページ遷移 useHistoryを使用

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,15 +1,23 @@
-import { Link } from "react-router-dom/cjs/react-router-dom"
+import { Link, useHistory } from "react-router-dom/cjs/react-router-dom"
 
 export const Page1 = () => {
     // ページ遷移時に渡すデータ
     const arr = [...Array(1000).keys()];
     console.log(arr);
+
+    const history = useHistory();
+
+    const onClickDetailA = () => history.push({ pathname: "/page1/detailA" ,state :arr});
+
     return (
         <div>
             <h1>ページ1です</h1>
             <Link to={{ pathname: "/page1/detailA" ,state :arr}}>DetailA</Link>
             <br />
             <Link to="/page1/detailB">DetailB</Link>
+            <br />
+            {/* ボタンをクリックしたら画面遷移する */}
+            <button onClick={onClickDetailA}>DetailA</button>
         </div>
     )
 }
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,19 +1,25 @@
 // State取得時に必要なライブラリ
-import { useLocation } from "react-router-dom/cjs/react-router-dom"
+import { useLocation, useHistory } from "react-router-dom/cjs/react-router-dom"
 
 export const Page1DetailA = () => {
     const location = useLocation();
-    console.log(location);
+    console.log(location.state);
+    const array = location.state;
+
+    const history = useHistory();
+
+    const onClickBack = () => history.goBack();
 
     return (
         <div>
             <h1>ページ1詳細Aです</h1>
             {/* Stateで取得したarrの中身をmap関数を使って一覧表示 */}
-            {location.state.map( value => (
-               <div>
+            {array.map( value => (
+               <div key={value}  >
                     <p>{value}</p>
                </div>
             ))}
+            <button onClick={onClickBack}>戻る</button>
         </div>
     )
 }
