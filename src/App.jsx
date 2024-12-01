a2eca92d71e1ce66a885a38190446f72b6485f89
yuuta-git12 <tayu2201@icloud.com>
2024-12-01 11:59:15 +0900

    ネストされたページ遷移の書き方追加

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import './App.css'
 import { Home } from './Home'
 import { Page1 } from './Page1'
 import { Page2 } from './Page2'
+import { Page1DetailA } from './Page1DetailA'
+import { Page1DetailB } from './Page1DetailB'
 
 function App() {
   
@@ -26,9 +28,21 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/page1">
-            <Page1 />
-          </Route>
+          <Route path="/page1" render={({match: {url}}) => (
+            <Switch>
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>    
+            )}
+          />
+          
           <Route path="/page2">
             <Page2 />
           </Route>
---
+++ src/Page1DetailA.jsx
@@ -0,0 +1,7 @@
+export const Page1DetailA = () => {
+    return (
+        <div>
+            <h1>ページ1詳細Aです</h1>
+        </div>
+    )
+}
