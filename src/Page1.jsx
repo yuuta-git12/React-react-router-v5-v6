3f5c5ff0083be5f01371f24d26ed41a434387906
yuuta-git12 <tayu2201@icloud.com>
2024-12-01 18:37:14 +0900

    画面遷移時の情報の受け渡し方法のサンプルコード

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom/cjs/react-router-dom"
 
 export const Page1 = () => {
+    // ページ遷移時に渡すデータ
+    const arr = [...Array(1000).keys()];
+    console.log(arr);
     return (
         <div>
             <h1>ページ1です</h1>
-            <Link to="/page1/detailA">DetailA</Link>
+            <Link to={{ pathname: "/page1/detailA" ,state :arr}}>DetailA</Link>
             <br />
             <Link to="/page1/detailB">DetailB</Link>
         </div>
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,19 @@
+// State取得時に必要なライブラリ
+import { useLocation } from "react-router-dom/cjs/react-router-dom"
+
 export const Page1DetailA = () => {
+    const location = useLocation();
+    console.log(location);
+
     return (
         <div>
             <h1>ページ1詳細Aです</h1>
+            {/* Stateで取得したarrの中身をmap関数を使って一覧表示 */}
+            {location.state.map( value => (
+               <div>
+                    <p>{value}</p>
+               </div>
+            ))}
         </div>
     )
 }
