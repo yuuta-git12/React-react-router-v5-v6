b148f0691f1a71825d7849cc97f1cdcf2ba225b6
yuuta-git12 <tayu2201@icloud.com>
2024-12-01 23:27:46 +0900

    [v5→v6修正]navigateとLinkに当てるpathとstateの書き方を修正

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -3,16 +3,16 @@ import { Link,useNavigate } from "react-router-dom";
 export const Page1 = () => {
     // ページ遷移時に渡すデータ
     const arr = [...Array(1000).keys()];
-    console.log(arr);
+    // console.log(arr);
 
     const navigate = useNavigate();
 
-    const onClickDetailA = () => navigate({ pathname: "/page1/detailA" ,state :arr});
+    const onClickDetailA = () => navigate("/page1/detailA" ,{state :arr});
 
     return (
         <div>
             <h1>ページ1です</h1>
-            <Link to={{ pathname: "/page1/detailA" ,state :arr}}>DetailA</Link>
+            <Link to="/page1/detailA"  state={arr}>DetailA</Link>
             <br />
             <Link to="/page1/detailB">DetailB</Link>
             <br />
